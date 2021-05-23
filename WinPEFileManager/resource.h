25653cb71783ef7017ac11b2ade07f5610e553f9
PCRepairTech286 <84412922+PCRepairTech286@users.noreply.github.com>
2021-05-23 20:12:20 +0300

    Add files via upload

---
+++ WinPEFileManager/resource.h
@@ -0,0 +1,57 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WinPEFileManager.rc
+//
+#define IDR_ACCELERATOR1                101
+#define IDI_FILEMANAGER                 102
+#define IDR_MAINMENU                    103
+#define APPTITLE                        104
+#define CANNOTCLOSE                     105
+#define IDB_BACKGROUND                  105
+#define RESTARTCONFIRMATION             106
+#define ABOUTTEXT                       109
+#define ABOUTTITLE                      110
+#define CURRENTDIRECTORY                1100
+#define ID_ACCELERATOR40001             40001
+#define ID_FILE_EXIT                    40002
+#define ID_HELP_ABOUT                   40003
+#define ID_RESTART                      40005
+#define ID_FILE_SHUTDOWN                40006
+#define ID_SHUTDOWN                     40007
+#define ID_ABOUT                        40008
+#define ID_FILE_OPENCMD                 40009
+#define ID_CMD                          40010
+#define ID_CMDOPEN                      40011
+#define NO_ACCELARATORS                 40012
+#define ID_FILE_OPENFILE                40014
+#define ID_7ZIP                         40015
+#define ID_40016                        40016
+#define ID_40017                        40017
+#define ID_ENGLISH                      40018
+#define ID_40019                        40019
+#define ID_ARABIC                       40020
+#define ID_40021                        40021
+#define ID_RUSSIAN                      40022
+#define ID_40023                        40023
+#define ID_HEBREW                       40024
+#define ID_40025                        40025
+#define ID_40026                        40026
+#define ID_40027                        40027
+#define ID_40028                        40028
+#define ID_LANGUAGE_ENGLISH             40029
+#define ID_LANGUAGE_40030               40030
+#define ID_LANGUAGE_40031               40031
+#define ID_LANGUAGE_40032               40032
+#define ID_HEBRRE                       40033
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40034
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
